2ff28de450ae0eda7e65f2dc96e13b1fcfb3a476
linyuanjin <linyuanjin@bytedance.com>
2019-08-08 18:36:52 +0800

    add crc16c; fix typo in tests/zbs/Makefile

--- src/terark/util/crc.hpp
+++ src/terark/util/crc.hpp
@@ -8,11 +8,21 @@ namespace terark {
 TERARK_DLL_EXPORT
 uint32_t Crc32c_update(uint32_t inCrc32, const void *buf, size_t bufLen);
 
+TERARK_DLL_EXPORT
+uint16_t Crc16c_update(uint16_t inCrc16, const void *buf, size_t bufLen);
+
 class TERARK_DLL_EXPORT BadCrc32cException : public BadChecksumException {
 public:
 	BadCrc32cException(fstring msg, uint32_t Old, uint32_t New)
 		: BadChecksumException(msg, Old, New) {}
 	~BadCrc32cException();
+};
+
+class TERARK_DLL_EXPORT BadCrc16cException : public BadChecksumException {
+public:
+    BadCrc16cException(fstring msg, uint16_t Old, uint16_t New)
+            : BadChecksumException(msg, Old, New) {}
+    ~BadCrc16cException();
 };
 
 } // terark
