66474539b03cda58f2a9019baa8acc04ce2f153b
leipeng <leipeng.tk@bytedance.com>
2019-12-13 15:44:12 +0800

    preproc: Add variadic macro TERARK_PP_CAT()

--- src/terark/preproc.hpp
+++ src/terark/preproc.hpp
@@ -16,6 +16,64 @@
                                             9,8,7,6,5,4,3,2,1,0)
 
 ///@{
+//#define TERARK_PP_CAT_0()       error "TERARK_PP_CAT" have at least 2 params
+// allowing TERARK_PP_CAT take just 1 argument
+#define TERARK_PP_CAT_0()
+#define TERARK_PP_CAT_1_1(x)       x
+#define TERARK_PP_CAT_1(x)       TERARK_PP_CAT_1_1(x)
+#define TERARK_PP_CAT_2(x,y)     TERARK_PP_CAT2(x,y)
+#define TERARK_PP_CAT_3(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_2(y,__VA_ARGS__))
+#define TERARK_PP_CAT_4(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_3(y,__VA_ARGS__))
+#define TERARK_PP_CAT_5(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_4(y,__VA_ARGS__))
+#define TERARK_PP_CAT_6(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_5(y,__VA_ARGS__))
+#define TERARK_PP_CAT_7(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_6(y,__VA_ARGS__))
+#define TERARK_PP_CAT_8(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_7(y,__VA_ARGS__))
+#define TERARK_PP_CAT_9(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_8(y,__VA_ARGS__))
+#define TERARK_PP_CAT_a(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_9(y,__VA_ARGS__))
+#define TERARK_PP_CAT_b(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_a(y,__VA_ARGS__))
+#define TERARK_PP_CAT_c(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_b(y,__VA_ARGS__))
+#define TERARK_PP_CAT_d(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_c(y,__VA_ARGS__))
+#define TERARK_PP_CAT_e(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_d(y,__VA_ARGS__))
+#define TERARK_PP_CAT_f(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_e(y,__VA_ARGS__))
+#define TERARK_PP_CAT_g(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_f(y,__VA_ARGS__))
+#define TERARK_PP_CAT_h(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_g(y,__VA_ARGS__))
+#define TERARK_PP_CAT_i(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_h(y,__VA_ARGS__))
+#define TERARK_PP_CAT_j(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_i(y,__VA_ARGS__))
+#define TERARK_PP_CAT_k(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_j(y,__VA_ARGS__))
+#define TERARK_PP_CAT_l(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_k(y,__VA_ARGS__))
+#define TERARK_PP_CAT_m(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_l(y,__VA_ARGS__))
+#define TERARK_PP_CAT_n(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_m(y,__VA_ARGS__))
+#define TERARK_PP_CAT_o(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_n(y,__VA_ARGS__))
+#define TERARK_PP_CAT_p(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_o(y,__VA_ARGS__))
+#define TERARK_PP_CAT_q(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_p(y,__VA_ARGS__))
+#define TERARK_PP_CAT_r(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_q(y,__VA_ARGS__))
+#define TERARK_PP_CAT_s(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_r(y,__VA_ARGS__))
+#define TERARK_PP_CAT_t(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_s(y,__VA_ARGS__))
+#define TERARK_PP_CAT_u(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_t(y,__VA_ARGS__))
+#define TERARK_PP_CAT_v(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_u(y,__VA_ARGS__))
+#define TERARK_PP_CAT_w(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_v(y,__VA_ARGS__))
+#define TERARK_PP_CAT_x(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_w(y,__VA_ARGS__))
+#define TERARK_PP_CAT_y(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_x(y,__VA_ARGS__))
+#define TERARK_PP_CAT_z(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_y(y,__VA_ARGS__))
+#define TERARK_PP_CAT_A(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_z(y,__VA_ARGS__))
+#define TERARK_PP_CAT_B(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_A(y,__VA_ARGS__))
+#define TERARK_PP_CAT_C(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_B(y,__VA_ARGS__))
+#define TERARK_PP_CAT_D(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_C(y,__VA_ARGS__))
+#define TERARK_PP_CAT_E(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_D(y,__VA_ARGS__))
+#define TERARK_PP_CAT_F(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_E(y,__VA_ARGS__))
+#define TERARK_PP_CAT_G(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_F(y,__VA_ARGS__))
+#define TERARK_PP_CAT_H(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_G(y,__VA_ARGS__))
+#define TERARK_PP_CAT_I(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_H(y,__VA_ARGS__))
+#define TERARK_PP_CAT_J(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_I(y,__VA_ARGS__))
+#define TERARK_PP_CAT_K(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_J(y,__VA_ARGS__))
+#define TERARK_PP_CAT_L(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_K(y,__VA_ARGS__))
+#define TERARK_PP_CAT_M(x,y,...) TERARK_PP_CAT2(x,TERARK_PP_CAT_L(y,__VA_ARGS__))
+///@}
+
+#define TERARK_PP_CAT(x,...) TERARK_PP_CAT2(x,TERARK_PP_CAT2 \
+       (TERARK_PP_CAT_,TERARK_PP_ARG_N(__VA_ARGS__))(__VA_ARGS__))
+
+///@{
 ///@param m map function
 ///@param c context
 #define TERARK_PP_MAP_0(m,c)
@@ -84,12 +142,15 @@
 
 /// @{ TERARK_PP_STR is a use case of TERARK_PP_MAP
 /// macro TERARK_PP_STR_2 is the 'map' function
+/// context of TERARK_PP_STR_2 is dummy
+///
 /// TERARK_PP_STR(a)     will produce: "a"
 /// TERARK_PP_STR(a,b,c) will produce: "a", "b", "c"
 /// so TERARK_PP_STR is a generic stringize macro
 #define TERARK_PP_STR_1(c,x) #x
 #define TERARK_PP_STR_2(c,x) TERARK_PP_STR_1(c,x)
 
+/// @note context for calling TERARK_PP_MAP is dummy(noted as '~')
 /// @param ... arg list to be stringized
 #define TERARK_PP_STR(...) TERARK_PP_MAP(TERARK_PP_STR_2,~, __VA_ARGS__)
 /// @}
--- tests/core/test_preproc.cpp
+++ tests/core/test_preproc.cpp
@@ -15,5 +15,20 @@ int main() {
             TERARK_PP_STR(1,2,3,4,5,6,7,8,9,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M)
     };
     printf("MAX_ARGS = %d\n", int(TERARK_PP_EXTENT(count)));
+
+    long long _1 = 1, _12 = 0x12, _123 = 0x123, _1234 = 0x1234, _12345 = 0x12345;
+    long long _123456789abcdef = 0x123456789abcdef;
+
+    long long TERARK_PP_CAT(_) = 0;
+
+    printf("_ = %#llx, _1 = %#llx, _12 = %#llx, _123 = %#llx, _1234 = %#llx, _1235 = %#llx\n",
+        TERARK_PP_CAT(_),
+        TERARK_PP_CAT(_,1),
+        TERARK_PP_CAT(_,1,2),
+        TERARK_PP_CAT(_,1,2,3),
+        TERARK_PP_CAT(_,1,2,3,4),
+        TERARK_PP_CAT(_,1,2,3,4,5));
+    printf("_123456789abcdef = %#llx\n", TERARK_PP_CAT(_,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f));
+
     return 0;
 }
