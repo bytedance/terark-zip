cd694f7c3c12a321eac8a3ca0a26152fe370d74f
leipeng <leipeng.tk@bytedance.com>
2020-02-04 16:42:16 +0800

    cspptrie.cpp: improve log

--- src/terark/fsa/cspptrie.cpp
+++ src/terark/fsa/cspptrie.cpp
@@ -3167,7 +3167,7 @@ void Patricia::TokenBase::mt_update(Patricia* trie1) {
             }
         }
         while (this != trie->m_dummy.m_link.next) {
-            fprintf(stderr, "DEBUG: wait for other thread set queue head as me(this = %p)\n", this);
+            fprintf(stderr, "DEBUG: very rare: wait for other thread set queue head as me(this = %p)\n", this);
         }
         // quick check m_acqseq
         if (trie->m_num_cpu_migrated * 8 >= trie->m_token_qlen ||
