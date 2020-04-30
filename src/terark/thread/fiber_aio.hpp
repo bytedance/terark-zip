2bb4925a8adbd54aa60a7346398a45bf38d13acf
leipeng <leipeng.tk@bytedance.com>
2020-04-30 16:33:44 +0800

    fix comment

--- src/terark/thread/fiber_aio.hpp
+++ src/terark/thread/fiber_aio.hpp
@@ -17,7 +17,7 @@ void fiber_aio_need(const void* buf, size_t len);
 TERARK_DLL_EXPORT
 ssize_t fiber_aio_write(int fd, const void* buf, size_t len, off_t offset);
 
-/// put the write to a dedicated thread to execute the write
+/// put the write to a dedicated thread to execute the write by aio
 TERARK_DLL_EXPORT
 ssize_t fiber_put_write(int fd, const void* buf, size_t len, off_t offset);
 
