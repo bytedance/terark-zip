b7c94444d8cc0a077b4e3f66dafa7d1fa51e5e1a
leipeng <leipeng.tk@bytedance.com>
2020-07-16 04:03:23 +0800

    test_patricia.cpp: use TERARK_VERIFY

--- tests/tries/test_patricia.cpp
+++ tests/tries/test_patricia.cpp
@@ -42,55 +42,67 @@ int main() {
     stdset.insert(key); \
     ret_ok = trie->insert(key, val_of(key), wtok); \
     val_got = aligned_load<char*>(wtok->value()); \
-    assert(val_got == val_obj); \
+    TERARK_VERIFY(val_got == val_obj); \
     trie->sync_stat(); \
-    assert(stdset.size() == trie->num_words()); \
+    TERARK_VERIFY(stdset.size() == trie->num_words()); \
     check_all(); \
   } while (0)
 
-  DO_INSERT("aaaabbbbcccc"); assert(ret_ok);
-  assert(trie->trie_stat().n_add_state_move == 1);
-  DO_INSERT("aaaabbbb"); assert(ret_ok); // split
-  assert(trie->trie_stat().n_split == 1);
+  DO_INSERT("aaaabbbbcccc"); TERARK_VERIFY(ret_ok);
+  TERARK_VERIFY(trie->trie_stat().n_add_state_move == 1);
+  DO_INSERT("aaaabbbb"); TERARK_VERIFY(ret_ok); // split
+  TERARK_VERIFY(trie->trie_stat().n_split == 1);
 
-  DO_INSERT("aaaabbbb"); assert(!ret_ok); // dup, fail
+  DO_INSERT("aaaabbbb"); TERARK_VERIFY(!ret_ok); // dup, fail
 
-  DO_INSERT("aaaac"); assert(ret_ok);
-  assert(trie->trie_stat().n_fork == 1);
+  DO_INSERT("aaaac"); TERARK_VERIFY(ret_ok);
+  TERARK_VERIFY(trie->trie_stat().n_fork == 1);
 
-  DO_INSERT("aaaad"); assert(ret_ok);
-  assert(trie->trie_stat().n_add_state_move == 2);
+  DO_INSERT("aaaad"); TERARK_VERIFY(ret_ok);
+  TERARK_VERIFY(trie->trie_stat().n_add_state_move == 2);
 
-  DO_INSERT("aaaade"); assert(ret_ok);
-  assert(trie->trie_stat().n_add_state_move == 3);
+  DO_INSERT("aaaade"); TERARK_VERIFY(ret_ok);
+  TERARK_VERIFY(trie->trie_stat().n_add_state_move == 3);
 
-  DO_INSERT("aaaa"); assert(ret_ok);
-  assert(trie->trie_stat().n_mark_final == 1);
+  DO_INSERT("aaaa"); TERARK_VERIFY(ret_ok);
+  TERARK_VERIFY(trie->trie_stat().n_mark_final == 1);
 
-  DO_INSERT(""); assert(ret_ok);
-  assert(trie->trie_stat().n_mark_final == 2);
+  DO_INSERT(""); TERARK_VERIFY(ret_ok);
+  TERARK_VERIFY(trie->trie_stat().n_mark_final == 2);
 
-  DO_INSERT("aaaabb"); assert(ret_ok);
-  assert(trie->trie_stat().n_split == 2);
+  DO_INSERT("aaaabb"); TERARK_VERIFY(ret_ok);
+  TERARK_VERIFY(trie->trie_stat().n_split == 2);
 
-  DO_INSERT("aaaab"); assert(ret_ok);
-  assert(trie->trie_stat().n_split == 3);
+  DO_INSERT("aaaab"); TERARK_VERIFY(ret_ok);
+  TERARK_VERIFY(trie->trie_stat().n_split == 3);
 
-  DO_INSERT("aaaabbbbccc"); assert(ret_ok);
-  assert(trie->trie_stat().n_split == 4);
+  DO_INSERT("aaaabbbbccc"); TERARK_VERIFY(ret_ok);
+  TERARK_VERIFY(trie->trie_stat().n_split == 4);
 
-  DO_INSERT("bb"); assert(ret_ok);
-  assert(trie->trie_stat().n_add_state_move == 4);
+  DO_INSERT("bb"); TERARK_VERIFY(ret_ok);
+  TERARK_VERIFY(trie->trie_stat().n_add_state_move == 4);
   size_t i = 0;
   for (char ch : fstring("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+-*/")) {
     char buf[16];
     snprintf(buf, sizeof(buf), "bb%c", ch);
     const char* dup = strdup(buf); // intentional leak memory
-    DO_INSERT(dup); assert(ret_ok);
+    DO_INSERT(dup); TERARK_VERIFY(ret_ok);
     TERARK_VERIFY_F(trie->trie_stat().n_add_state_move == 5 + i, "%zd %zd",
                     trie->trie_stat().n_add_state_move ,  5 + i);
     i++;
   }
+
+  DO_INSERT("cca"); TERARK_VERIFY(ret_ok);
+  TERARK_VERIFY(trie->trie_stat().n_add_state_move == 5+66);
+  DO_INSERT("ccb"); TERARK_VERIFY(ret_ok);
+  TERARK_VERIFY(trie->trie_stat().n_fork == 2);
+
+  DO_INSERT("ddaa"); TERARK_VERIFY(ret_ok);
+  TERARK_VERIFY(trie->trie_stat().n_add_state_move == 5+66+1);
+  DO_INSERT("dda"); TERARK_VERIFY(ret_ok);
+  TERARK_VERIFY(trie->trie_stat().n_split == 5);
+  DO_INSERT("ddab"); TERARK_VERIFY(ret_ok);
+  TERARK_VERIFY(trie->trie_stat().n_add_state_move == 5+66+2);
 
   wtok->release();
   iter->dispose();
