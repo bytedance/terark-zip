3034c92c19272c44acf1170aa5e9d18744907984
leipeng <leipeng.tk@bytedance.com>
2020-07-15 22:09:55 +0800

    Add tests/tries/test_patricia.cpp

---
+++ tests/tries/test_patricia.cpp
@@ -0,0 +1,50 @@
+//
+// Created by leipeng on 2020/7/15.
+//
+#include <terark/fsa/cspptrie.hpp>
+
+using namespace terark;
+
+int main() {
+  std::unique_ptr<Patricia> trie(
+      Patricia::create(sizeof(void*), 4<<20, Patricia::MultiWriteMultiRead));
+  
+  bool ret_ok = false;
+  const char* val_obj = NULL;
+  const char* val_got = NULL;
+  auto val_of = [&](const char* x) { val_obj = x; return &val_obj; };
+  auto wtok = trie->tls_writer_token_nn();
+  wtok->acquire(trie.get());
+
+#define DO_INSERT(key) do { \
+    ret_ok = trie->insert(key, val_of(key), wtok); \
+    val_got = aligned_load<char*>(wtok->value()); \
+    assert(val_got == val_obj); \
+    trie->sync_stat(); \
+  } while (0)
+  
+  DO_INSERT("aaaabbbbcccc"); assert(ret_ok);
+  DO_INSERT("aaaabbbb"); assert(ret_ok); // split
+  assert(trie->trie_stat().n_split == 1);
+
+  DO_INSERT("aaaabbbb"); assert(!ret_ok); // dup, fail
+
+  DO_INSERT("aaaac"); assert(ret_ok);
+  assert(trie->trie_stat().n_fork == 1);
+
+  DO_INSERT("aaaad"); assert(ret_ok);
+  assert(trie->trie_stat().n_add_state_move == 1);
+
+  DO_INSERT("aaaade"); assert(ret_ok);
+  assert(trie->trie_stat().n_add_state_move == 2);
+
+  DO_INSERT("aaaa"); assert(ret_ok);
+  assert(trie->trie_stat().n_mark_final == 1);
+
+  DO_INSERT(""); assert(ret_ok);
+  assert(trie->trie_stat().n_mark_final == 2);
+
+  wtok->release();
+
+  return 0;
+}
