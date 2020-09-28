3f287b0ed4b11fd5a2afa53fb4f7dce90ce47a89
guokuankuan <guokuankuan@bytedance.com>
2020-09-28 18:06:14 +0800

    add build_cmake.sh

---
+++ build_cmake.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+BASE=$PWD
+OUTPUT=output
+mkdir -p $OUTPUT
+
+if test -n "$BUILD_BRANCH"; then
+    # this script is run in SCM auto build
+    git checkout "$BUILD_BRANCH"
+    sudo apt-get update
+    sudo apt-get install libaio-dev
+else
+    echo you must ensure libaio-dev have been installed
+fi
+
+git submodule update --init --recursive
+
+cd $BASE/$OUTPUT && cmake ../ -DCMAKE_BUILD_TYPE=Release
+cd $BASE/$OUTPUT && make -j $(nproc) && make install
